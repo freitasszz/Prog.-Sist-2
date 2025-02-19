bc420c860a8582127a0a8f601fc828dcdaae8352
Gabriel Freitas <gfcmiwa@gmail.com>
2025-02-19 23:40:39 +0000

    lab02

---
+++ Atv 1/src/lab02/src/ContaInfinita.java
@@ -0,0 +1,18 @@
+package banco;
+
+public class ContaInfinita extends Conta{
+    public ContaInfinita(int n, double s){
+        super(n,s);
+    }
+    @Override
+    public void depositar(double valor){
+        saldo += + 5;
+
+    }
+    @Override
+    public boolean sacar(double valor){
+        saldo -= valor;
+        return true;
+    }
+
+}
---
+++ Atv 1/src/lab02/src/Main.java
@@ -0,0 +1,18 @@
+import banco.Conta;
+import banco.ContaBancaria;
+import banco.ContaInfinita;
+
+class Main{
+    public static void main (String [] args ){
+        Conta[] contas ={ new ContaInfinita(1111,50),
+                          new ContaBancaria(2222, 99),
+                          new ContaBancaria(3333, 1000),
+                          new ContaEspecial(4444, 800, 200)};
+        for (int i=0; i<contas.length; i++){
+            contas[i].depositar(10);
+            contas[i].sacar(900);
+            System.out.println(contas[i]);
+        }
+    }
+}
+
